8f241da149e2877ded1543d18a00552f64a02fcf
kevin <kevinmolina240206@gmail.com>
2025-08-28 09:50:53 -0500

    actualizacion

--- Balones/Program.cs
+++ Balones/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+Balones balon = new Balones();
+
+balon.id = 1;
+balon.peso = 5;
+/*balon.FechaCreacion = DateTime.now;*/
+balon.Marca = new Marcas() { Id = 1, Nombre = "adidas" };
+balon.deporte = new Deportes() { Id = 1, Deporte = "Baloncesto" };
+balon.estadoBalon = new EstadosBalon() {Id = 1, Estado = "En perfectas condiciones"};
+
+Console.WriteLine(balon.id);
+Console.WriteLine(balon.Marca.Nombre);
+Console.WriteLine(balon.deporte.Deporte);
+Console.WriteLine(balon.estadoBalon.Estado);
+
+public class EstadosBalon
+{
+    public int Id;
+    public string? Estado;
+}
+public class Deportes
+{
+    public int Id;
+    public string? Deporte;
+
+}
+public class Balones
+{
+    public int id;
+    public decimal peso;
+    /*public DateTime FechaCreacion;*/
+    public Marcas? Marca;
+    public Deportes? deporte;
+    public EstadosBalon? estadoBalon;
+
+}
+public class Marcas
+{
+    public int Id;
+    public string? Nombre;
+
+}
